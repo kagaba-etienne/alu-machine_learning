846810542b907cef417e4e4fa7549fa05a859ddc
kagaba-etienne <kagabaetienne04@gmail.com>
2026-05-19 20:40:23 +0200

    1-neuron

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -16,6 +16,18 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if (nx < 1):
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
